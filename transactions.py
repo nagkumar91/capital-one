fb19883a6bb64332aad4fec52507529a6b247d72
Nagkumar <me@nagkumar.com>
2017-01-11 11:24:37 -0800

    Added crystal ball feature

--- transactions.py
+++ transactions.py
@@ -11,7 +11,7 @@ AUTH_TOKEN = os.environ.get("AUTH_TOKEN", None)
 API_TOKEN = os.environ.get("API_TOKEN", None)
 
 
-def get_all_transactions(ignore_donuts):
+def prepare_request():
     if USER_ID:
         user_id = USER_ID
     else:
@@ -37,6 +37,11 @@ def get_all_transactions(ignore_donuts):
             "json-verbose-mode": False
         }
     }
+    return headers, data
+
+
+def get_all_transactions(ignore_donuts):
+    headers, data = prepare_request()
     response = requests.post("https://2016.api.levelmoney.com/api/v2/core/get-all-transactions", data=json.dumps(data),
                              headers=headers)
     if response.status_code == 200:
@@ -52,6 +57,27 @@ def get_all_transactions(ignore_donuts):
         return filtered_json_response
     else:
         click.echo("Something went wrong in downloading the transactions.")
+
+
+def add_crystal_ball_transactions(all_transactions, ignore_donuts):
+    headers, data = prepare_request()
+    data['month'] = 1
+    data['year'] = 2017
+    response = requests.post("https://2016.api.levelmoney.com/api/v2/core/projected-transactions-for-month",
+                             data=json.dumps(data),
+                             headers=headers)
+    if response.status_code == 200:
+        json_response = response.json()['transactions']
+        for transaction in json_response:
+            transaction['transaction-time'] = datetime.strptime(transaction['transaction-time'],
+                                                                "%Y-%m-%dT%H:%M:%S.%fZ")
+            if ignore_donuts:
+                if transaction['merchant'] == "Krispy Kreme Donuts" or transaction['merchant'] == "DUNKIN #336784":
+                    continue
+            all_transactions.append(transaction)
+    else:
+        click.echo("unable to get crystal ball transactions.")
+    return all_transactions
 
 
 def get_income_and_expenditure_for_month(month, year, all_transactions):
@@ -76,8 +102,11 @@ def get_income_and_expenditure_for_month(month, year, all_transactions):
 
 @click.command()
 @click.option('--ignore-donuts', is_flag=True, default=False)
-def transactions(ignore_donuts):
+@click.option('--crystal-ball', is_flag=True, default=False)
+def transactions(ignore_donuts, crystal_ball):
     all_transactions = get_all_transactions(ignore_donuts)
+    if crystal_ball:
+        all_transactions = add_crystal_ball_transactions(all_transactions, ignore_donuts)
     start_date = datetime(2014, 10, 01)
     end_date = datetime(2017, 01, 31)
     all_transactions_for_averages = []
